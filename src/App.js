51118622ad176e6c3ed3a25ef4ac32fa3072cfe2
codlyf <codlyfcoding@gmail.com>
2023-08-28 21:33:35 +0530

    ui changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header1 from './components/Header1';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+          <Header1/> 
+          <Home />
+    </div>
+  );
+}
+
+export default App;
